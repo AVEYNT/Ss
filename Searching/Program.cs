f151cf5b78ecbeb55f26057b3aa68a46d88fa2d4
ddvlp <daisukikamu@gmail.com>
2022-05-15 17:13:27 +0700

    Add project files.

---
+++ Searching/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+class Search
+{
+    static int [] data = new int[4] { 10, 24, 3, 5 };
+    static int x;
+
+    static void Main (String [] args)
+    {
+        Console.WriteLine ("Masukkan nilai yang dicari= ");
+        x = Int32.Parse(Console.ReadLine());
+
+        cari(x, data);
+    }
+    private static void cari ( int x, int [] data)
+    {
+        for(int i=0; i<data.Length; i++)
+        {
+            if (data[i] == x)
+            {
+                Console.WriteLine("Data Ditemukan");
+            }
+        }
+    }
+}
